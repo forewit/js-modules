19932f8ed949cbd3135c7ae35c51e0e82c7fb125
Anderson <MDAnderson@samaritan.org>
2021-06-25 15:37:52 -0400

    na

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-// testing keys.js
+// TESTING KEYS.JS
 keys.on('17 65', ()=>{console.log('ctrl + a')})
 keys.start()
 
-// testing Gestures.js
-let gesturesElm = document.getElementById('gestures');
-let testGestures = new Gestures(gesturesElm);
-testGestures.on('pinching', ()=>{console.log('pinching'); gesturesElm.innerHTML="pinching"})
-testGestures.on('touchDragStart', ()=>{console.log('touch drag start'); gesturesElm.innerHTML="touch drag start"})
-testGestures.on('touchDragging', ()=>{console.log('touch dragging'); gesturesElm.innerHTML="touch dragging"})
-testGestures.on('touchDragEnd', ()=>{console.log('touch drag end'); gesturesElm.innerHTML="touch drag end"})
-testGestures.on('longpress', ()=>{console.log('longpress'); gesturesElm.innerHTML="longpress"})
-testGestures.on('tap', ()=>{console.log('tap'); gesturesElm.innerHTML="tap"})
-testGestures.on('doubleTap', ()=>{console.log('double tap'); gesturesElm.innerHTML="double tap"})
-testGestures.on('click', ()=>{console.log('click'); gesturesElm.innerHTML="click"})
-testGestures.on('doubleClick', ()=>{console.log('double click'); gesturesElm.innerHTML="double click"})
-testGestures.on('longClick', ()=>{console.log('long click'); gesturesElm.innerHTML="long click"})
-testGestures.on('rightClick', ()=>{console.log('right click'); gesturesElm.innerHTML="right click"})
-testGestures.on('mouseDragging', ()=>{console.log('mouse dragging'); gesturesElm.innerHTML="mouse dragging"})
-testGestures.on('mouseDragEnd', ()=>{console.log('mouse drag end'); gesturesElm.innerHTML="mouse drag end"})
-testGestures.on('longpressDragStart', ()=>{console.log('longpress drag start'); gesturesElm.innerHTML="longpress drag start"})
-testGestures.on('longpressDragging', ()=>{console.log('longpress dragging'); gesturesElm.innerHTML="longpress dragging"})
-testGestures.on('longpressDragEnd', ()=>{console.log('longpress drag end'); gesturesElm.innerHTML="longpress drag end"})
-testGestures.start();
 
-// testing Draggable.js
-new Draggable(document.getElementById('draggable'), {placeholderClass:"placeholder"});
-
-// testing files.js
+// TESTING FILES.JS
 // alternate -- files.newDropZone(document.getElementById('drop-region'));
 let dropzoneElm = document.getElementById('files');
 new files.Dropzone(
@@ -36,17 +13,19 @@ new files.Dropzone(
     {hoverClass:"dragover"}
 );
 
+// TESTING GESTURES.JS
+let newgesturesElm = document.getElementById("gestures");
+gestures.track(newgesturesElm);
+
+newgesturesElm.addEventListener("gesture", function(e) {
+    console.log(e.detail.name, e.detail.data);
+});
+
 // testing pen.js
 new Pen(document.getElementById('pen'));
 
 // testing Toolbar.js
 let toolbar = new Toolbar(document.getElementById("toolbar"), true);
 
-
-// testing newgestures.js
-let newgesturesElm = document.getElementById("newgestures");
-gestures.track(newgesturesElm);
-
-newgesturesElm.addEventListener("gesture", function(e) {
-    console.log(e.detail.name, e.detail.data);
-});
+// testing Draggable.js
+new Draggable(document.getElementById('draggable'), {placeholderClass:"placeholder"});
