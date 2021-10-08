a9015d07191fa565335a01ed3290b99e3fdab2a7
Anderson <MDAnderson@samaritan.org>
2021-10-08 16:55:02 -0400

    na

--- modules/gestures.js
+++ modules/gestures.js
@@ -262,8 +262,8 @@
 
 
         // don't handle multiple touches if already tracking a touch
-        if (e.targetTouches.length > 1) {
-            if (e.targetTouches[0].identifier == touch.identifier) return;
+        if (e.touches.length > 1) {
+            if (e.touches[0].identifier == touch.identifier) return;
             pinching = true;
         }
 
@@ -272,7 +272,7 @@
         window.addEventListener('touchcancel', touchendHandler);
 
         // update primary touch location
-        touch = copyTouch(e.targetTouches[0]);
+        touch = copyTouch(e.touches[0]);
         activeTouchElm = this;
 
         // longpress DETECTION
@@ -302,7 +302,7 @@
             let lastX = touch.x,
                 lastY = touch.y;
 
-            touch = copyTouch(e.targetTouches[0]);
+            touch = copyTouch(e.touches[0]);
 
             // calculate change in x and y from last touch event
             let dx = touch.x - lastX,
@@ -314,9 +314,9 @@
                 dispatchGesture(activeTouchElm, e, { name: "touch-dragging", x: touch.x, y: touch.y, dx: dx, dy: dy });
             }
             return;
-        } else if (!longpressed && (pinching || e.targetTouches.length > 1)) {
-            touch = copyTouch(e.targetTouches[0]);
-            let touch2 = copyTouch(e.targetTouches[1]);
+        } else if (!longpressed && (pinching || e.touches.length > 1)) {
+            touch = copyTouch(e.touches[0]);
+            let touch2 = copyTouch(e.touches[1]);
             let center = {
                 x: (touch.x + touch2.x) / 2,
                 y: (touch.y + touch2.y) / 2
@@ -343,12 +343,12 @@
             dragging = true;
             if (longpressed) {
                 dispatchGesture(activeTouchElm, e, { name: "longpress-drag-start", x: touch.x, y: touch.y })
-                touch = copyTouch(e.targetTouches[0]);
+                touch = copyTouch(e.touches[0]);
                 dispatchGesture(activeTouchElm, e, { name: "longpress-dragging", x: touch.x, y: touch.y, dx: 0, dy: 0 });
 
             } else {
                 dispatchGesture(activeTouchElm, e, { name: "touch-drag-start", x: touch.x, y: touch.y });
-                touch = copyTouch(e.targetTouches[0]);
+                touch = copyTouch(e.touches[0]);
                 dispatchGesture(activeTouchElm, e, { name: "touch-dragging", x: touch.x, y: touch.y, dx: 0, dy: 0 });
             }
         }
@@ -356,8 +356,8 @@
 
     function touchendHandler(e) {
         if (dragging &&
-            e.targetTouches.length > 0 &&
-            e.targetTouches[0].identifier == touch.identifier) {
+            e.touches.length > 0 &&
+            e.touches[0].identifier == touch.identifier) {
             return;
         }
 
